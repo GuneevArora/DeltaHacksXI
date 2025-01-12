bb76d3a74393193ecdabdd38aedf81a24e495093
KamS04 <singhkam07@gmail.com>
2025-01-12 03:36:44 -0500

    Fixed IconBtn

--- react_components/__init__.py
+++ react_components/__init__.py
@@ -4,5 +4,5 @@ import streamlit.components.v1 as components
 
 _icon_btn = components.declare_component('iconbtn', url='http://localhost:3001/icon_btn')
 def icon_btn(src: str, key: str) -> bool:
-    return _icon_btn(src=src, key=key)
+    return _icon_btn(src=src, key=key, value=False)
 
--- react_components/frontend/src/IconBtn.tsx
+++ react_components/frontend/src/IconBtn.tsx
@@ -13,17 +13,23 @@ function IconBtn({ args }: ComponentProps): ReactElement {
     });
 
     useEffect(() => {
+        if (src == "/email_icon.png") {
+            console.log(`LEAK BTN STATE ${clicked}`);
+        }
         if (clicked) {
             setTimeout(() => {
-                // Streamlit.setComponentValue(false);
-                setClicked(false);                
-            }, 0);
+                Streamlit.setComponentValue(0);
+                setClicked(false);
+                if (src == '/email_icon.png') {
+                    console.log('RESET BIN STATE');
+                }
+            }, 1000);
         }
     }, [clicked]);
 
     return (
         <div className="w-full flex justify-center items-center transition transform hover:scale-110 p-4" style={{height: `${HEIGHT}px`}}>
-            <button className="flex justify-center items-center w-full aspect-square border border-2 rounded-lg"  onClick={() => { Streamlit.setComponentValue(true); setClicked(true); }}>
+            <button className="flex justify-center items-center w-40 aspect-square border border-2 rounded-lg"  onClick={() => { Streamlit.setComponentValue(1); setClicked(true); }}>
                 <img style={{maxWidth: "50%", maxHeight: "50%"}} src={src}>
                 </img>
             </button>
