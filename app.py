9fb05c2eaea74aa350a4dfdbce2479cd1bc81606
KamS04 <singhkam07@gmail.com>
2025-01-12 02:35:48 -0500

    LETS GO

--- app.py
+++ app.py
@@ -1,21 +1,66 @@
-
+from typing import NamedTuple
 import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
 import cohere
 from react_components import icon_btn
 
-
-
 if 'sidebar_state' not in st.session_state:
     st.session_state['sidebar_state'] = 'none'
+if 'skip' not in st.session_state:
+    st.session_state['skip'] = False
+
+st.markdown('''
+<style>
+    div[data-testid="stSidebarCollapseButton"] {
+        display: none!important;
+    }
+        
+    div[data-testid="stSidebarUserContent"] {
+        padding: 0px 0px !important;
+    }
+</style>
+''', unsafe_allow_html=True)
+
+EXPANDED_STYLING = '''
+<style>
+    .stSidebar {
+        width: 300x!important;
+        max-width: 300px!important;
+        min-width: 300px!important;
+    }
+</style>
+'''
+COLAPSED_STYLING = '''
+<style>
+    .stSidebar {
+        width: 100px!important;
+        max-width: 100px!important;
+        min-width: 100px!important;
+    }
+</style>
+'''
+
+
+SIDE_BAR_STYLING = None
+if st.session_state['sidebar_state'] == 'none':
+    SIDE_BAR_STYLING = COLAPSED_STYLING
+else:
+    SIDE_BAR_STYLING = EXPANDED_STYLING
 
 def change_sidebar(tab: str):
     st.session_state['sidebar_state'] = tab
 
 def toggle_sidebar(tab: str):
+    global SIDE_BAR_STYLING
+    if st.session_state['skip']:
+        st.session_state['skip'] = False
+        return
     if st.session_state['sidebar_state'] == tab:
         st.session_state['sidebar_state'] = 'none'
     else:
         st.session_state['sidebar_state'] = tab
+    st.session_state['skip'] = True
+    st.rerun()
 
 # Custom components
 st.title("Security (Optional) Bot")
@@ -52,40 +97,23 @@ if prompt := st.chat_input("What is up?"):
 icon_col, tab_col = None, None
 
 if st.session_state['sidebar_state'] == 'none':
-    icon_col,tab_col = st.sidebar.columns([75/280, 1 - 75/280])
+    icon_col = st.sidebar.columns([1])[0]
 else:
-    icon_col = st.sidebar.columns([1])
+    icon_col,tab_col = st.sidebar.columns([100/300, 1 - 100/300])
+
+
+# (icon,key,tab)
+Tab = NamedTuple('Tab', [('icon',str),('tab',str)])
+TABS = [
+    Tab('/email_icon.png','leak'),
+    Tab('/password_icon.png','pwman'),
+    Tab('/url_icon.png','safe'),
+    Tab('/vault_icon.png','vault'),
+]
 
 with icon_col:
-    if icon_btn(src='/icon.png'):
-        toggle_sidebar('goggle')
+    for icon,tab in TABS:
+        if icon_btn(src=icon, key=tab):
+            toggle_sidebar(tab)
 
-if st.session_state['sidebar_state'] == 'none':
-    st.markdown('''
-    <style>
-        .sidebar-content {
-            width: 80px;
-        }
-    </style>
-    ''')
-else:
-    st.markdown('''
-    <style>
-        .sidebar-content {
-            width: 300px;
-        }
-    </style>
-    ''')
-
-
-# Custom CSS
-st.markdown(
-    """
-    <style>
-        section[data-testid="stSidebar"] {
-            width: 600px !important; # Set the width to your desired value
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+st.markdown(SIDE_BAR_STYLING, unsafe_allow_html=True)
