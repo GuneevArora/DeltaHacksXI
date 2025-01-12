e1ed316ac0c89f8143deeb11a956f7e8c0d99cfb
GuneevArora <122828231+GuneevArora@users.noreply.github.com>
2025-01-11 22:49:52 -0500

    url safety checker

---
+++ url_checker.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from pysafebrowsing import SafeBrowsing
+
+def check_url_safety(url):
+    s = SafeBrowsing(st.secrets["SAFE_BROWSING_API_KEY"], )
+    r = s.lookup_urls([url])
+    print(r[url])
+
+
+check_url_safety("http://www.malware.testing.google.test/testing/malware/")  # test the function
+
+check_url_safety("http://testsafebrowsing.appspot.com/s/unwanted.html")  # test the function
+
+check_url_safety("www.google.com")  # test the function
+
+
